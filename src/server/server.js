a0a48e57363b9ac35bd7ead9a39a288d34bdc928
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2013-09-05 22:12:30 +0100

    First client/server test working

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,17 @@
 "use strict";
 
-exports.number = function(){
-  return 3;
+var http = require("http");
+var server = http.createServer();
+
+exports.start = function(){
+
+  server.on("request", function(request, response){
+    response.end();
+  });
+
+  server.listen(8080);
+};
+
+exports.stop = function(callback){
+  server.close(callback);
 };
